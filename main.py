f05683c6b2e33742e8b2b40043cae49c51aa72ba
hammerklavier <hammerklavier@noreply.gitcode.com>
2024-07-21 00:35:02 +0800

    v1.0.3 bug fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
     Add Tracker Url to Magnet Url
 """
 
-import os
+import os, sys
 import MagnetLinkGenerator as mlg
 
 original_magnet_link = input("Initial Magnet link:\t")
@@ -11,11 +11,11 @@ urls: list[str] = []
 
 while True:
     try:
-        url = input("Tracker Url (Press `Ctrl + D` to stop):\t")
+        url = input("Tracker Url (Press `Ctrl + D(Unix)/Z(Windows)` to stop):\t")
         if url:
             urls.append(url)
     except EOFError:
         merged_megnet_link = mlg.add_tracker(original_magnet_link, *urls)
         os.system("clear")
-        print(f"{'-' * 10}Magnet Link with Tracker: {'-' * 10}\n{merged_megnet_link}")
-        exit(0)
+        print(f"{'-' * 10} Magnet Link with Tracker: {'-' * 10}\n{merged_megnet_link}")
+        sys.exit(0)
